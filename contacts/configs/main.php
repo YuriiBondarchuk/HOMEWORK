1c8aa8a376607861f32e7cd9b568a416f47112b8
Yurii_Bondarchuck <ymbondarchuck@gmail.com>
2018-03-05 12:01:08 +0200

    Added file .gitinore '.idea'

--- contacts/configs/main.php
+++
@@ -1,9 +0,0 @@
-<?php
-//var_dump(dirname(__DIR__));
-$basePath=dirname(__DIR__);
-
-return ['baseUrl'=>'/homework/contacts',
-        'controllersPath'=>"{$basePath}/controllers",
-        'viewsPath'=>"{$basePath}/views",
-        'storagePath'=>"{$basePath}/storages"
-];

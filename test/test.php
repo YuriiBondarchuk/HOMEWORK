2ba63b1f521689c0f6a01d04e8b16885b933d55c
YuriiBondarchuk <ymbondarchuk@gmail.com>
2018-08-21 18:57:52 +0300

    working

--- test/test.php
+++ test/test.php
@@ -1,9 +1,29 @@
 <?php
-$str = 'Doors Kangaroo';
-function strposm($str){
-    $arr = explode(' ',$str'0
-    './');
- /'//'''''''''''''''''''//'/';[[[[[[[[[[[[[[[[[[[[[;[''/////////////////////////////////////////}   var_dump($arr);
-    67
+$str = 'Doorrsyyy Kaangaroott';
+function strposm($str)
+{
+    $temp_char = [];
+    $arr = explode(' ', $str);
+    foreach ($arr as $value) {
+        $temp = str_split($value);
+        foreach ($temp as $key => $value) {
+
+            if ($key != 0) {
+                if ($temp[$key] === $temp[$key - 1]) {
+                    array_push($temp_char, $temp[$key] . $temp[$key - 1]);
+
+
+                }
+            }
+        }
+    }
+    var_dump($temp_char);
+//    echo 'longest common part :';
+//    echo "</br>";
+//    foreach (array_unique($temp_char) as $value) {
+//        echo "$value ";
+//        echo "</br>";
+//    }
+}
 
 strposm($str);
---
+++ test/test_test.php
@@ -0,0 +1,17 @@
+<?php
+$str = 'Doorrsyyyy';
+
+function strposm($str)
+{
+     $temp_char=[];
+    for ($i = 0; $i < strlen($str);$i++) {
+        $temp_char[$i]=$str[$i];
+//    while($str[$i]==$str[$i+1]){
+//        $temp_char[$i].=$str[$i+1];
+//        $i++;
+//    }
+    }
+//  cd
+}
+
+strposm($str);
